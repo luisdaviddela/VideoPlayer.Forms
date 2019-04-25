7c6a62d0a23cb744c42b5280f9cc4692df8e8462
david.cruz <delacruz_luis@outlook.es>
2019-04-25 15:47:48 -0500

    Video Player Android & iOS

--- VideoPlayer/VideoPlayer/VideoPlayer.iOS/AppDelegate.cs
+++ VideoPlayer/VideoPlayer/VideoPlayer.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Octane.Xamarin.Forms.VideoPlayer.iOS;
 using UIKit;
 
 namespace VideoPlayer.iOS
@@ -24,7 +25,7 @@ namespace VideoPlayer.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            FormsVideoPlayer.Init();
             return base.FinishedLaunching(app, options);
         }
     }
